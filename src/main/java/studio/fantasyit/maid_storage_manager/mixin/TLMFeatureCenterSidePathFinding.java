d89b0d5899675396954ec6b62f25a3dc57208ca2
xypp <2952795729@qq.com>
2025-08-06 20:38:03 +0800

    修复mixin

--- src/main/java/studio/fantasyit/maid_storage_manager/mixin/TLMFeatureCenterSidePathFinding.java
+++ src/main/java/studio/fantasyit/maid_storage_manager/mixin/TLMFeatureCenterSidePathFinding.java
@@ -11,7 +11,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class TLMFeatureCenterSidePathFinding {
     @Redirect(
             method = "<init>(Lnet/minecraft/world/level/pathfinder/NodeEvaluator;Lnet/minecraft/server/level/ServerLevel;Lcom/github/tartaricacid/touhoulittlemaid/entity/passive/EntityMaid;FI)V",
-            at = @At(value = "INVOKE", target = "Lcom/github/tartaricacid/touhoulittlemaid/entity/passive/EntityMaid;blockPosition()Lnet/minecraft/core/BlockPos;")
+            at = @At(value = "INVOKE", target = "Lcom/github/tartaricacid/touhoulittlemaid/entity/passive/EntityMaid;blockPosition()Lnet/minecraft/core/BlockPos;", remap = true),
+            remap = false
     )
     public BlockPos modify(EntityMaid maid) {
         return maid.hasRestriction() ? maid.getRestrictCenter() : maid.blockPosition();
