700a62a7bd5c347ef9cc67f81dc7e4e06f765bad
xypp <2952795729@qq.com>
2025-09-18 18:22:47 +0800

    分离TourGuide的内容至其他mod

--- src/main/java/studio/fantasyit/tour_guide/context/IGuiContext.java
+++
@@ -1,4 +0,0 @@
-package studio.fantasyit.tour_guide.context;
-
-public interface IGuiContext {
-}
--- src/main/java/studio/fantasyit/tour_guide/mark/IMark.java
+++
@@ -1,7 +0,0 @@
-package studio.fantasyit.tour_guide.mark;
-
-import net.minecraft.resources.ResourceLocation;
-
-public interface IMark {
-    ResourceLocation getId();
-}
