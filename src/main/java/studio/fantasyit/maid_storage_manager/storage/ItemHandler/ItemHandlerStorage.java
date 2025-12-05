03bd65cb70beb3975011e0fb85fa00acb7946b0a
xypp <2952795729@qq.com>
2025-12-06 02:09:36 +0800

    支持识别可排序的容器

--- src/main/java/studio/fantasyit/maid_storage_manager/storage/ItemHandler/ItemHandlerStorage.java
+++ src/main/java/studio/fantasyit/maid_storage_manager/storage/ItemHandler/ItemHandlerStorage.java
@@ -5,7 +5,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.Capabilities;
@@ -58,8 +60,11 @@ public class ItemHandlerStorage implements IMaidStorage {
         return new AbstractFilterableBlockStorage();
     }
 
+    public static TagKey<Block> SORTABLE = TagKey.create(ForgeRegistries.BLOCKS.getRegistryKey(), new ResourceLocation(MaidStorageManager.MODID, "sortable_chests"));
     @Override
     public @Nullable ISortSlotContext onStartSorting(ServerLevel level, EntityMaid maid, Target target) {
-        return new AbstractItemHandlerContext();
+        if(level.getBlockState(target.getPos()).is(SORTABLE))
+            return new AbstractItemHandlerContext();
+        return null;
     }
 }
